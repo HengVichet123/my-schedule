86ee61d27264e57240f37f8ea32d8eed79810af9
HengVichet123 <h.vichet123@gmail.com>
2026-05-08 02:02:05 +0900

    Bump cache v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'schedule-v3';
+const CACHE = 'schedule-v4';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon.png'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
