76fe2886ed137600c8421756b80b2906fd9ff2c0
HengVichet123 <h.vichet123@gmail.com>
2026-05-08 02:02:38 +0900

    Delete entire repeat series when Apply-all is checked

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'schedule-v4';
+const CACHE = 'schedule-v5';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon.png'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
