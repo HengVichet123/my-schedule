58ee53b14ab9ae00a0c1700c28ff014570ed28b5
HengVichet123 <h.vichet123@gmail.com>
2026-05-08 02:19:58 +0900

    SW: skipWaiting + clients.claim so updates apply immediately

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'schedule-v8';
+const CACHE = 'schedule-v9';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon.png'];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('install', e => e.waitUntil(
+  caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+));
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
