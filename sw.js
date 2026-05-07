18cdac6f38fa2e9cf9eea2a76b1b8b3eb99cc118
HengVichet123 <h.vichet123@gmail.com>
2026-05-08 02:17:08 +0900

    Add current time red line indicator, updates every 30s

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'schedule-v7';
+const CACHE = 'schedule-v8';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon.png'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
